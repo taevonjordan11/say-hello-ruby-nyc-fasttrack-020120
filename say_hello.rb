7fa545245f3f5e099ce2ff0c0f2ddd36d0f5106b
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-03 23:00:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
-def say_hello(language = "Ruby", name)
+def say_hello(language = "Ruby", name = "Hello Ruby Programmer!")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
 puts say_hello
